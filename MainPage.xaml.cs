ba7bc0d0a19e030107959f46187427af7485e20c
Jay Beavers <jay@hikinghomeschoolers.org>
2014-02-17 09:11:45 -0800

    Initial import

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Controls;
+
+namespace JayBeavers.ChronoZoom
+{
+    public sealed partial class MainPage
+    {
+
+        private Uri _currentUri;
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ChronoZoomWebView.NavigationCompleted += ChronoZoomWebView_NavigationCompleted;
+ 
+            DataTransferManager.GetForCurrentView().DataRequested += MainPage_DataRequested;
+        }
+
+        void MainPage_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            if (_currentUri == null) return;
+
+            var request = args.Request;
+            request.Data.Properties.Title = "I'd like to share this timeline '" + ChronoZoomWebView.DocumentTitle + "' with you...";
+            request.Data.Properties.Description = "Share timeline '" + ChronoZoomWebView.DocumentTitle + "'";
+            request.Data.SetWebLink(_currentUri);
+            request.Data.Properties.ContentSourceWebLink = _currentUri;
+        }
+
+        void ChronoZoomWebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            _currentUri = args.Uri;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChronoZoom for Windows")]
+[assembly: AssemblyProduct("ChronoZoom for Windows")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: ComVisible(false)]
